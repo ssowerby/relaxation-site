bc10da984f4967d12faff9edbf0b9c6ff8898bbe
Steve Sowerby <steve.sowerby@gmail.com>
2022-01-16 19:57:49 +0000

    Shrink logo once scrolled down Fix copyright to have current year

---
+++ assets/js/main.js
@@ -0,0 +1,14 @@
+if (
+    "IntersectionObserver" in window &&
+    "IntersectionObserverEntry" in window &&
+    "intersectionRatio" in window.IntersectionObserverEntry.prototype
+) {
+    let observer = new IntersectionObserver(entries => {
+        if (entries[0].isIntersecting) {
+            document.body.classList.remove("header-not-at-top");
+        } else {
+            document.body.classList.add("header-not-at-top");
+        }
+    });
+    observer.observe(document.querySelector("#topbox"));
+}
